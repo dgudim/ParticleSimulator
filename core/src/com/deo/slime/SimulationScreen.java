2200891f66d1f16121e6e4d00b0c3304548b2c0f
dgudim <dgudim@gmail.com>
2021-05-15 18:05:07 +0300

    added support for threading

--- core/src/com/deo/slime/SimulationScreen.java
+++ core/src/com/deo/slime/SimulationScreen.java
@@ -23,9 +23,9 @@ import static java.lang.Math.pow;
 import static java.lang.Math.random;
 
 public class SimulationScreen extends GenericScreen {
-
+    
     Agent[] agents;
-
+    
     Pixmap gridPixmap;
     static volatile float[][] gridPixmapColors;
     Texture gridTexture;
@@ -34,26 +34,31 @@ public class SimulationScreen extends GenericScreen {
     int currentSimulationRule_firstHalf = 13;
     int currentSimulationRule_secondHalf = 13;
     int currentlySelectedSimulationRule = 13;
-
+    
+    boolean threading = false;
+    int numberOfThreads = 4;
+    int batchSize = numberOfAgents / numberOfThreads;
+    boolean active = false;
+    
     static float mapDivider = 1;
-
+    
     static int trailMapHeight;
     static int trailMapWidth;
-
+    
     int frame = 0;
     boolean recording = false;
     boolean bloom = true;
     boolean showAgents = false;
-
+    
     final boolean randomSpawn = true;
     float evaporationRate = 0.3f;
-
+    
     float maxTrailIntensity = 0;
-
+    
     PostProcessor blurProcessor;
-
+    
     FileHandle externalParams;
-
+    
     float[][] availableRules = new float[][]{
             {2f, 50, 7, 2, 40, 0, 1, 1, 0.035f},
             {2f, 50, 9, 19, 50, 0, 1, 1, 0.035f},
@@ -69,25 +74,25 @@ public class SimulationScreen extends GenericScreen {
             {-3f, -20, 12, -13, 0, 15, 1, 1, 0.035f},
             {3, 45, 3, 3, 3, 0, 1, 1, 0.035f},
             {3, 30, 3, 3, 45, 0, 1, 1, 0.035f}};
-
+    
     float[] params;
-
+    
     Color[][] availablePalettes = new Color[][]{
             {Color.BLACK, Color.ORANGE, Color.CYAN, Color.CORAL},
             {Color.BLACK, Color.valueOf("#662341"), Color.valueOf("#ffe240"), Color.FIREBRICK},
             {Color.BLACK, Color.ORANGE, Color.RED, Color.GRAY},
             {Color.BLACK, Color.LIME, Color.TEAL, Color.CLEAR},
             {Color.BLACK, Color.SKY, Color.BLACK, Color.BLACK, Color.TEAL, Color.BLACK}};
-
+    
     SimulationScreen(Game game, float[] params) {
         externalParams = Gdx.files.local("params.txt");
-
+        
         loadParams();
         trailMapHeight = (int) (HEIGHT / mapDivider);
         trailMapWidth = (int) (WIDTH / mapDivider);
-
+        
         init(game, trailMapWidth, trailMapHeight);
-
+        
         ShaderLoader.BasePath = "shaders/";
         blurProcessor = new PostProcessor(WIDTH, HEIGHT, false, false, true);
         Bloom bloom = new Bloom(WIDTH, HEIGHT);
@@ -95,7 +100,7 @@ public class SimulationScreen extends GenericScreen {
         bloom.setBloomIntensity(1f);
         bloom.setBloomSaturation(1f);
         blurProcessor.addEffect(bloom);
-
+        
         agents = new Agent[numberOfAgents];
         for (int i = 0; i < agents.length; i++) {
             Agent newAgent = new Agent(false);
@@ -109,24 +114,57 @@ public class SimulationScreen extends GenericScreen {
             newAgent.rotation = (float) (random() * 360);
             agents[i] = newAgent;
         }
-
+        
         this.params = params;
-
+        
         if (params == null) {
             this.params = availableRules[currentlySelectedSimulationRule];
         }
         changeParamsForAllAgents(this.params);
-
+        
         gridPixmap = new Pixmap(trailMapWidth, trailMapHeight, Format.RGBA8888);
         gridPixmapColors = new float[trailMapWidth][trailMapHeight];
         gridTexture = new Texture(gridPixmap);
+        
+        if (threading) {
+            for (int t = 0; t < numberOfThreads; t++) {
+                final int finalT = t;
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        while (true) {
+                            if (active) {
+                                for (int i = 0; i < batchSize; i++) {
+                                    agents[finalT * batchSize + i].update(0.05f);
+                                }
+                            }
+                        }
+                    }
+                }).start();
+            }
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (true) {
+                        if (!showAgents) {
+                            fade();
+                        }
+                        copyOver();
+                        if (showAgents) {
+                            fade();
+                        }
+                    }
+                }
+            }).start();
+            active = true;
+        }
     }
-
+    
     @Override
     public void show() {
-
+    
     }
-
+    
     void loadParams() {
         if (!externalParams.exists()) {
             externalParams.writeString("fieldSizeDivider_1", false);
@@ -149,9 +187,9 @@ public class SimulationScreen extends GenericScreen {
             }
         }
     }
-
+    
     void spreadInCirce(int type) {
-
+        
         for (Agent agent : agents) {
             double deg = random();
             double dist = random();
@@ -184,26 +222,26 @@ public class SimulationScreen extends GenericScreen {
                     break;
             }
         }
-
+        
     }
-
+    
     @Override
     public void render(float delta) {
-
+        
         super.render(delta);
-
+        
         final float relativeDelta;
-        if (recording) {
+        if (recording && !threading) {
             relativeDelta = 0.8f;
         } else {
             relativeDelta = delta / 0.16f;
         }
-
+        
         if (Gdx.input.isKeyJustPressed(Input.Keys.R)) {
             recording = !recording;
             frame = 0;
         }
-
+        
         if (Gdx.input.isKeyJustPressed(Input.Keys.F1)) {
             for (Agent agent : agents) {
                 agent.x = trailMapWidth / 2f;
@@ -211,13 +249,13 @@ public class SimulationScreen extends GenericScreen {
                 agent.rotation = (float) (random() * 360);
             }
         }
-
+        
         for (int i = 2; i < 10; i++) {
             if (Gdx.input.isKeyJustPressed(Input.Keys.valueOf("F" + i))) {
                 spreadInCirce(i - 2);
             }
         }
-
+        
         if (Gdx.input.isKeyJustPressed(Input.Keys.H)) {
             for (int x = 0; x < trailMapWidth; x++) {
                 for (int y = 0; y < trailMapHeight; y++) {
@@ -225,39 +263,39 @@ public class SimulationScreen extends GenericScreen {
                 }
             }
         }
-
+        
         if (Gdx.input.isKeyJustPressed(Input.Keys.NUM_1)) {
             currentSimulationRule_firstHalf = currentlySelectedSimulationRule;
             changeParamsForHalfAgents(params, false);
         }
-
+        
         if (Gdx.input.isKeyJustPressed(Input.Keys.NUM_2)) {
             currentSimulationRule_secondHalf = currentlySelectedSimulationRule;
             changeParamsForHalfAgents(params, true);
         }
-
+        
         if (Gdx.input.isKeyJustPressed(Input.Keys.W)) {
             currentlySelectedSimulationRule++;
             changeSimulationRules();
         }
-
+        
         if (Gdx.input.isKeyJustPressed(Input.Keys.S)) {
             currentlySelectedSimulationRule--;
             changeSimulationRules();
         }
-
+        
         if (Gdx.input.isKeyJustPressed(Input.Keys.C)) {
             currentPalette++;
         }
-
+        
         if (Gdx.input.isKeyJustPressed(Input.Keys.B)) {
             bloom = !bloom;
         }
-
+        
         if (Gdx.input.isKeyJustPressed(Input.Keys.J)) {
             showAgents = !showAgents;
         }
-
+        
         if (Gdx.input.isKeyPressed(Input.Keys.F)) {
             for (int i = 0; i < 5; i++) {
                 for (Agent agent : agents) {
@@ -266,7 +304,7 @@ public class SimulationScreen extends GenericScreen {
                 fade();
             }
         }
-
+        
         if (bloom) {
             blurProcessor.capture();
         }
@@ -277,19 +315,21 @@ public class SimulationScreen extends GenericScreen {
         if (bloom) {
             blurProcessor.render();
         }
-
-        if (showAgents) {
-            fade();
+        
+        if (!threading) {
+            if (showAgents) {
+                fade();
+            }
+            for (Agent agent : agents) {
+                agent.update(relativeDelta);
+            }
+            if (!showAgents) {
+                fade();
+            }
+            copyOver();
         }
-        for (Agent agent : agents) {
-            agent.update(relativeDelta);
-        }
-        if (!showAgents) {
-            fade();
-        }
-        copyOver();
-
-        if (recording) {
+        
+        if (recording && !threading) {
             if (bloom) {
                 makeAScreenShot(frame);
             } else {
@@ -297,26 +337,26 @@ public class SimulationScreen extends GenericScreen {
             }
             frame++;
         }
-
+        
         batch.begin();
         font.draw(batch, currentSimulationRule_firstHalf + "/" + currentSimulationRule_secondHalf + " [" + currentlySelectedSimulationRule + "] ", 30, 30);
         batch.end();
-
+        
         if (Gdx.input.isKeyJustPressed(Input.Keys.E)) {
             game.setScreen(new SettingsScreen(game, params));
             this.dispose();
         }
     }
-
+    
     public static float getFromMap(int x, int y) {
         try {
             return gridPixmapColors[x][y];
         } catch (Exception e) {
             return 0;
         }
-
+        
     }
-
+    
     void fade() {
         for (int x = 1; x < trailMapWidth - 1; x++) {
             for (int y = 1; y < trailMapHeight - 1; y++) {
@@ -329,18 +369,18 @@ public class SimulationScreen extends GenericScreen {
                 gridPixmapColors[x][y] = sum / 9f;
             }
         }
-
+        
         for (int x = 0; x < trailMapWidth; x++) {
             for (int y = 0; y < trailMapHeight; y++) {
                 gridPixmapColors[x][y] = max(gridPixmapColors[x][y] - evaporationRate, 0);
             }
         }
     }
-
+    
     void copyOver() {
         for (int x = 0; x < trailMapWidth; x++) {
             for (int y = 0; y < trailMapHeight; y++) {
-
+                
                 switch (currentPalette) {
                     case (0):
                         float ratio1 = (gridPixmapColors[x][y] - maxTrailIntensity / 2f) * 2;
@@ -374,7 +414,7 @@ public class SimulationScreen extends GenericScreen {
             }
         }
     }
-
+    
     void changeSimulationRules() {
         if (currentlySelectedSimulationRule == availableRules.length) {
             currentlySelectedSimulationRule = 0;
@@ -384,13 +424,13 @@ public class SimulationScreen extends GenericScreen {
         }
         params = availableRules[currentlySelectedSimulationRule];
     }
-
+    
     void changeParamsForAllAgents(float[] params) {
         changeParamsForHalfAgents(params, false);
         changeParamsForHalfAgents(params, true);
         evaporationRate = params[8];
     }
-
+    
     void changeParamsForHalfAgents(float[] params, boolean lastHalf) {
         int start = 0;
         int end = agents.length / 2;
@@ -408,34 +448,34 @@ public class SimulationScreen extends GenericScreen {
             agent.sensorAngleOffset2 = (int) params[5];
             agent.maxPheromoneTrailConcentration = params[6];
             agent.pheromoneDepositRate = params[7];
-
+            
         }
         maxTrailIntensity = agents[0].maxPheromoneTrailConcentration;
         for (Agent agent : agents) {
             maxTrailIntensity = max(maxTrailIntensity, agent.maxPheromoneTrailConcentration);
         }
     }
-
+    
     @Override
     public void resize(int width, int height) {
         super.resize(width, height);
     }
-
+    
     @Override
     public void pause() {
-
+    
     }
-
+    
     @Override
     public void resume() {
-
+    
     }
-
+    
     @Override
     public void hide() {
-
+    
     }
-
+    
     @Override
     public void dispose() {
         super.dispose();
